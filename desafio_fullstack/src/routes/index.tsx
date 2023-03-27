f1fc28aa40dd6506e5fec97ec356a2fe0842fa23
naoeoneto <abdpneto@gmail.com>
2023-03-26 21:26:51 -0300

    fix: add vercel.json

--- desafio_fullstack/src/routes/index.tsx
+++ desafio_fullstack/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/Login";
 import RegisterPage from "../pages/Register";
 
